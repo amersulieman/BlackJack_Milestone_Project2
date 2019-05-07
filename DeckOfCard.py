62021af896983ed6271023c032087a866f5c301c
amersulieman <aksuliem@uno.edu>
2019-05-07 13:10:26 -0500

    shuffle deck added

--- DeckOfCard.py
+++ DeckOfCard.py
@@ -3,27 +3,36 @@
     Uses composition of Card object
 '''
 from Card import Card
+from random import shuffle
+
 class DeckOfCards:
+    #cards 4 kinds of suits
     suits = ["Clubs","Diamonds","Hearts","Spades"]
-    def __init__(self,nums):
-        self.numOfCards = nums
+    cardsAmnt = 52
+    def __init__(self):
         self.deck=list()
 
     def createCards(self):
+            #For every suit, we have 13 cards : [ACE, 2-10, Jack, Queen, King]
         for suit in self.suits:
+            #ace created
             ace = Card(suit,"Ace")
+            self.deck.append(ace)
+            for num in range(2,11):
+                #cards between 2-10 created
+                newCard=Card(suit,num)
+                self.deck.append(newCard)
+            #jack,queen,king are created
             jack = Card(suit,"Jack")
             queen = Card(suit,"Queen")
             king = Card(suit,"King")
-            self.deck.append(ace)
-            for num in range(2,11):
-                newCard=Card(suit,num)
-                self.deck.append(newCard)
             self.deck.append(jack)
             self.deck.append(queen)
             self.deck.append(king)
     def __len__(self):
-        return self.numOfCards
-
-deck = DeckOfCards(52)
+        return self.cardsAmnt
+        
+#create deck of cards
+deck = DeckOfCards()
 deck.createCards()
+shuffle(deck.deck)
