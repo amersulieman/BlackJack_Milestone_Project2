bf5f6a0e8ec438ceb758bc4b4a753b5db5fb69f1
amersulieman <aksuliem@uno.edu>
2019-05-07 13:44:53 -0500

    Player represntation Done

---
+++ Player.py
@@ -0,0 +1,14 @@
+'''
+    A representtion of the player in the game
+'''
+from random import randint
+class Player:
+    def __init__(self):
+        self.bankRole = 500
+        self.cardSum = 0
+    
+    def placeBet(self):
+        betAmount = randint(1,self.bankRole+1)
+        return betAmount
+    
+    
